3360b567eadda58d2929c5cc586b29426f5bbbfc
Marc-Antoine Lacasse <m.a.l.musique@gmail.com>
2025-07-28 20:57:05 -0400

    feat(asset): Add layout page
    Implement some sass tools to improve the workflow

--- assets/admin.js
+++ assets/admin.js
@@ -1,7 +1,1 @@
 import './css/app.scss';
-
-import * as Turbo from "@hotwired/turbo"
-
-
-// Enable Turbo.js when the application is ready
-Turbo.start();
--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,7 @@
 import './css/app.scss';
+
+import * as Turbo from "@hotwired/turbo"
+
+
+// Enable Turbo.js when the application is ready
+Turbo.start();
